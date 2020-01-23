a9a20891cfc98bf6ade8450d99eabb2c7d597c9c
Saurabh Singh <sighsaurabh920@gmail.com>
2020-01-23 11:04:06 +0000

    project modified

--- src/main/java/org/worldbuild/es/EsApplication.java
+++ src/main/java/org/worldbuild/es/EsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WbEsProjectApplication {
+public class EsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WbEsProjectApplication.class, args);
+		SpringApplication.run(EsApplication.class, args);
 	}
 
 }
